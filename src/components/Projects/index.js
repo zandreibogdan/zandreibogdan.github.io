7e48c784f87f3ac02678d86444c7f391882b3b74
zandreibogdan <zandreibogdan24@gmail.com>
2022-05-08 19:00:02 +0300

    updating projs cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState, useEffect } from 'react'
 import instaproject from '../../assets/images/insta-project.png'
+import tenzies from '../../assets/images/tenzies.png'
 
 export default function Projects() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -47,16 +48,20 @@ export default function Projects() {
             <a href="https://instaggram-cloned.netlify.app/">
               <img src={instaproject} alt="insta-project" />
             </a>
-            <h3 className="portfolio-card-title">Project title</h3>
-            <p className="portfolio-card-p">Project description</p>
+            <h3 className="portfolio-card-title">Instagram clone</h3>
+            <p className="portfolio-card-p">
+              Made using Next.js, Firebase v9, Tailwind CSS and more
+            </p>
           </div>
 
           <div className="portfolio-card">
-            <a href="https://instaggram-cloned.netlify.app/">
-              <img src={instaproject} alt="insta-project" />
+            <a href="https://zandreibogdan.github.io/tenzies/">
+              <img src={tenzies} alt="insta-project" />
             </a>
-            <h3 className="portfolio-card-title">Project title</h3>
-            <p className="portfolio-card-p">Project description</p>
+            <h3 className="portfolio-card-title">Tenzies</h3>
+            <p className="portfolio-card-p">
+              Made as a way to train and improve React.js skills{' '}
+            </p>
           </div>
 
           <div className="portfolio-card">
